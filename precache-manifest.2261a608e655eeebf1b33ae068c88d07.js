77560849ec5eaa4d3f3df1d20a3ff1491136c136
Victor Nunez <vnunez0914@gmail.com>
2020-01-01 17:53:02 -0600

    Updates

---
+++ precache-manifest.2261a608e655eeebf1b33ae068c88d07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d1a4534ee46047cc612ce43290d63d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "9594a7f9fdb74d040320",
+    "url": "/clicky-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cb1ac36c64c35305a656",
+    "url": "/clicky-game/static/js/2.8ccbfd2d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.8ccbfd2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9594a7f9fdb74d040320",
+    "url": "/clicky-game/static/js/main.95b2e96c.chunk.js"
+  },
+  {
+    "revision": "76da5bcc0e2cfb61c97a",
+    "url": "/clicky-game/static/js/runtime-main.39dcd64a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
